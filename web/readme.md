bb6739c21547c38f8fa5530168203906a38573bf
Lars Wegner <lars.wegner2@de.bosch.com>
2022-03-08 16:22:06 +0100

    Add a simple web app for chain suggestion
    - The web app is based on vanila typescript and is independant of any
      framework, which can be later easily integrated into any of the other
      project web pages.
    
    - Add command for generating Keplr wallet config.
    
    - Use "testnet" in the chain id and "demo" in the token name to
      differentiate it from the future mainnet.
    
    Signed-off-by: Lars Wegner <lars.wegner2@de.bosch.com>

---
+++ web/readme.md
@@ -0,0 +1,10 @@
+#Prerequisits
+## Install nodejs with npm
+Suggestion is to use [Node Version Manager](https://github.com/nvm-sh/nvm#installing-and-updating) for installing nodejs
+
+# Build and run the web app
+```shell
+npm install
+npm run build && npx http-server dist
+```
+
---
+++ web/vite.config.ts
@@ -0,0 +1,32 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { Buffer } from 'buffer'
+import { defineConfig } from 'vite'
+import { dynamicImport } from 'vite-plugin-dynamic-import'
+import envCompatible from 'vite-plugin-env-compatible'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    global: {
+      Buffer: Buffer
+    }
+  },
+  server: {
+    watch: {
+      ignored: [
+        '!**/node_modules/@starport/vue/src/**',
+        '!**/node_modules/@starport/vuex/src/**'
+      ]
+    }
+  },
+  plugins: [ nodeResolve(), dynamicImport(), envCompatible()],
+  optimizeDeps: {
+    include: [
+      'gradient-avatar',
+      'crypto-js',
+      'axios',
+      'qrcode',
+      '@cosmjs/encoding'
+    ]
+  }
+})
