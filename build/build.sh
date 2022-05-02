fa8a95c8fb9e103777a1931320caa1fdedb0cead
Lars Wegner <lars.wegner2@de.bosch.com>
2022-05-02 14:30:18 +0200

    Update the docker file to include the new web app
    Signed-off-by: Lars Wegner <lars.wegner2@de.bosch.com>

--- build/build.sh
+++ build/build.sh
@@ -2,3 +2,4 @@
 
 
 DOCKER_BUILDKIT=1 docker build --progress=plain --build-arg  BRANCH=main --secret id=ssh_key,src=./ssh_key --target  node -t esc-backbone .
+DOCKER_BUILDKIT=1 docker build --progress=plain --build-arg  BRANCH=main --secret id=ssh_key,src=./ssh_key --target  web-chain-suggestion -t esc-web  .
