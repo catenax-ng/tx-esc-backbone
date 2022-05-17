7ebb13da54eca805a5408f77907eec0326d76fdd
Lars Wegner <lars.wegner2@de.bosch.com>
2022-05-17 21:30:40 +0200

    Add testnet home folder into Docker image temporarily
    - Due to missing access to persistent volumes, the home folder of the validator node is contained in the Docker image.
    - Source code is now copied from enclosing folder and not checked out anymore.
    - Add github workflow to push the images.
    
    Signed-off-by: Lars Wegner <lars.wegner2@de.bosch.com>

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_LOCATION=$( dirname -- "${BASH_SOURCE[0]}")
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --build-arg  BRANCH=main --secret id=ssh_key,src=./ssh_key --target  node -t esc-backbone .
-DOCKER_BUILDKIT=1 docker build --progress=plain --build-arg  BRANCH=main --secret id=ssh_key,src=./ssh_key --target  web-chain-suggestion -t esc-web  .
+
+DOCKER_BUILDKIT=1 docker build --progress=plain --target  esc-backbone-node -t esc-backbone -f "$SCRIPT_LOCATION/Dockerfile" "$SCRIPT_LOCATION/.."
+DOCKER_BUILDKIT=1 docker build --progress=plain --target  esc-backbone-web -t esc-web  -f "$SCRIPT_LOCATION/Dockerfile" "$SCRIPT_LOCATION/.."
