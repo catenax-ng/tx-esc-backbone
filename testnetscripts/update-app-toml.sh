fda839efc6f8e93099fef0c9bd128cd83905ca43
Lars Wegner <lars.wegner2@de.bosch.com>
2022-06-29 09:14:47 +0200

    Manually enable apis
    - enable rest api
    - allow all clients to rpc
    - enable swagger
    - disable cors for grpc
    - make tendermint listen on every interface
    - expose 9090 and 9091 on validator image
    
    Signed-off-by: Lars Wegner <lars.wegner2@de.bosch.com>

--- testnetscripts/update-app-toml.sh
+++ testnetscripts/update-app-toml.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
 CONFIG_FILE_NAME="config/app.toml"
+
+function toml_set(){
+  echo not working yet
+  exit 2
+  FILE=${1:?"TOML file required"}
+  SECTION=${2:?"SECTION required"}
+  KEY=${3:?"KEY required"}
+  VALUE=${4:?"VALUE required"}
+  sed -i "s/(\[$SECTION\][^\[]\{0,\}\s\{0,\})\#\{0,\}\s\{0,\}$KEY\s\{0,\}=.\{0,\}/\1\n$KEY = $VALUE/"
+}
+
+function toml_get(){
+  echo not working yet
+  exit 2
+  FILE=${1:?"TOML file required"}
+  SECTION=${2:?"SECTION required"}
+  KEY=${3:?"KEY required"}
+  VALUE=${4:?"VALUE required"}
+  cat $FILE | sed -n "s/\[$SECTION\][^\[]\{0,\}\s\{0,\}\#\{0,\}\s\{0,\}$KEY\s\{0,\}=\s\{0,\}(.\{0,\})\n/\1/p"
+  grep -e"\[$SECTION\][^\[]\{0,\}\s\{0,\}\#\{0,\}\s\{0,\}$KEY\s\{0,\}=\s\{0,\}(.\{0,\})\n"
+}
+
+function enable_rest() {
+  TRG=${1:?"Target folder required"}
+
+  toml_set "${TRG%/}/$CONFIG_FILE_NAME" "api" "enable" "true"
+  toml_set "${TRG%/}/$CONFIG_FILE_NAME" "api" "swagger" "true"
+  toml_set "${TRG%/}/$CONFIG_FILE_NAME" "api" "enabled-unsafe-cors" "true"
+}
+
 function update_app_toml(){
   SRC=${1:?"Source folder required"}
   TRG=${2:?"Target folder required"}
 
+  enable_rest "$TRG"
+  toml_set "${TRG%/}/$CONFIG_FILE_NAME" "grpc-web" "enabled-unsafe-cors" "true"
 }
--- testnetscripts/update-config-toml.sh
+++ testnetscripts/update-config-toml.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
 CONFIG_FILE_NAME="config/config.toml"
+
+function toml_set(){
+  echo not working yet
+  exit 2
+  FILE=${1:?"TOML file required"}
+  SECTION=${2:?"SECTION required"}
+  KEY=${3:?"KEY required"}
+  VALUE=${4:?"VALUE required"}
+  sed -i "s/(\[$SECTION\][^\[]\{0,\}\s\{0,\})\#\{0,\}\s\{0,\}$KEY\s\{0,\}=.\{0,\}/\1\n$KEY = $VALUE/"
+}
+
+function toml_get(){
+  echo not working yet
+  exit 2
+  FILE=${1:?"TOML file required"}
+  SECTION=${2:?"SECTION required"}
+  KEY=${3:?"KEY required"}
+  VALUE=${4:?"VALUE required"}
+  cat $FILE | sed -n "s/\[$SECTION\][^\[]\{0,\}\s\{0,\}\#\{0,\}\s\{0,\}$KEY\s\{0,\}=\s\{0,\}(.\{0,\})\n/\1/p"
+  grep -e"\[$SECTION\][^\[]\{0,\}\s\{0,\}\#\{0,\}\s\{0,\}$KEY\s\{0,\}=\s\{0,\}(.\{0,\})\n"
+}
+function publish_rpc(){
+  TRG=${1:?"Target folder required"}
+  toml_set "${TRG%/}/$CONFIG_FILE_NAME" "rpc" "laddr" "\"tcp://127.0.0.1:26657\""
+  toml_set "${TRG%/}/$CONFIG_FILE_NAME" "rpc" "cors_allowed_origins" "[\"*\"]"
+}
 function update_persistent_peers() {
   SRC=${1:?"Source folder required"}
   TRG=${2:?"Target folder required"}
@@ -8,10 +34,10 @@ function update_persistent_peers() {
   REPLACEMENT="$(cat ${SRC%/}/$CONFIG_FILE_NAME | grep "persistent_peers = \"")"
   REPLACEMENT=${REPLACEMENT//\//\\/}
   REPLACEMENT=${REPLACEMENT//:/\\:}
-  sed -i "s/\s\{0,\}\#\{0,\}\s\{0,\}persistent_peers\s\{0,\}=.*/$REPLACEMENT/" "${TRG%/}/$CONFIG_FILE_NAME"
+  sed -i "s/\s\{0,\}\#\{0,\}\s\{0,\}persistent_peers\s\{0,\}=.\{0,\}/$REPLACEMENT/" "${TRG%/}/$CONFIG_FILE_NAME"
 }
 
-
+# cors_allowed_origins = ["*"]
 function update_config_toml(){
   SRC=${1:?"Source folder required"}
   TRG=${2:?"Target folder required"}
