7d0c4b3026a457e2f19433954b2baa9c948564dc
Lars Wegner <lars.wegner2@de.bosch.com>
2022-08-31 23:45:21 +0200

    Write env variable starting with 'WEBAPP_' to chain/env.json
    - giving ownership  to /usr/share/nginx/html so that the env.json can be
     written on startup
    
    Signed-off-by: Lars Wegner <lars.wegner2@de.bosch.com>

--- build/webapp-entrypoint.sh
+++ build/webapp-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_LOCATION=$( dirname -- "${BASH_SOURCE[0]}")
 
-
+${SCRIPT_LOCATION%/}/write-env-to-json.sh ${ENV_JSON:-/usr/share/nginx/html/chain/env.json}
 ${SCRIPT_LOCATION%/}/update-chain-suggestion.sh ${CHAIN_SUGGESTION:-/usr/share/nginx/html/chain/catenax-testnet-1-suggestion.json}
 # entrypoint of https://github.com/nginxinc/docker-nginx-unprivileged/
 /docker-entrypoint.sh "$@"
---
+++ build/write-env-to-json.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script reads the environment variables and writes the one with the prefix 'WEBAPP_' to the given JSON file.
+# This enables the static web app to access these environment variables by doing a HTTP GET.
+# The image works in Docker and Kubernetes by just passing environment variables to the deployed container.
+# This omits the necessity to mount the config file, which is quite tool dependent.
+JSON_FILE=${1:?"File name to store env variables required."}
+PREFIX='WEBAPP_'
+
+# start json object
+echo "{" > $JSON_FILE
+while IFS= read -r line; do
+    echo $line | \
+    # filter on variables with the prefix
+    grep -e "^$PREFIX"  | \
+    # skip non matching variables
+    grep -E '^.+$' | \
+    # transform variable into json attribute and write it to the file
+    sed -r "s/^([^=\s]*)=(.*)/\"\1\":\"\2\",/g" >> $JSON_FILE
+done <<< $(env)
+# end json object
+echo "}" >> $JSON_FILE
+
+# remove comma after last variable
+sed -i -r ":begin;$!N;s/,\n}/\n}/;tbegin;P;D" $JSON_FILE
