fb558e2812381d40c16478c29a4d45aaeb1debe4
Lars Wegner <lars.wegner2@de.bosch.com>
2022-09-08 16:37:53 +0200

    Add docker build stage for the faucet
    Signed-off-by: Lars Wegner <lars.wegner2@de.bosch.com>

--- build/build.sh
+++ build/build.sh
@@ -7,3 +7,5 @@ DOCKER_BUILDKIT=1 docker build --progress=plain --target  esc-backbone-node -t e
 docker tag esc-backbone-node ghcr.io/catenax-ng/esc-backbone-node:latest
 DOCKER_BUILDKIT=1 docker build --progress=plain --target  esc-backbone-web -t esc-backbone-web  -f "$SCRIPT_LOCATION/Dockerfile" "$SCRIPT_LOCATION/.."
 docker tag esc-backbone-web ghcr.io/catenax-ng/esc-backbone-web:latest
+DOCKER_BUILDKIT=1 docker build --progress=plain --target  esc-backbone-faucet -t catena-faucet  -f "$SCRIPT_LOCATION/Dockerfile" "$SCRIPT_LOCATION/.."
+docker tag esc-backbone-faucet ghcr.io/catenax-ng/catena-faucet:latest
--- build/load-images-into-kind.sh
+++ build/load-images-into-kind.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 kind load docker-image ghcr.io/catenax-ng/esc-backbone-node:latest
 kind load docker-image ghcr.io/catenax-ng/esc-backbone-web:latest
+kind load docker-image ghcr.io/catenax-ng/catena-faucet:latest
