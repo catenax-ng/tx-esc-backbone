6893dfdbe209309682b4eb49e0fab8b8eb48f733
manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-05-16 03:05:09 +0200

    Add sell token commands in test script
    Signed-off-by: manoranjith <ponraj.manoranjitha@in.bosch.com>

--- test/scripts/ubc_msg_init_test.sh
+++ test/scripts/ubc_msg_init_test.sh
@@ -53,3 +53,8 @@ echo "\n# ubc buy - value in vouchers\n\n"
 esc-backboned query bank balances $ubc_trader
 esc-backboned tx ubc buytokens "10000uvoucher" --from $ubc_trader --yes
 esc-backboned query bank balances $ubc_trader
+
+echo "\n# ubc sell - value in tokens\n\n"
+esc-backboned query bank balances $ubc_trader
+esc-backboned tx ubc selltokens "10000ucax" --from=$ubc_trader --yes
+esc-backboned query bank balances $ubc_trader
