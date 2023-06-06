6675637607911bbd3e3a00fc00dcac2cc1f90a75
manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-06-06 14:55:17 +0200

    Add license header for ubc module files
    Signed-off-by: manoranjith <ponraj.manoranjitha@in.bosch.com>

--- x/ubc/genesis.go
+++ x/ubc/genesis.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2022-2023 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository at
+// https://github.com/catenax-ng/product-esc-backbone-code
+//
+// SPDX-License-Identifier: Apache-2.0
 package ubc
 
 import (
