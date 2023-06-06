febb27f13cbe7c64302c4adce869d22c575558ed
manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-06-06 10:33:18 +0200

    Scaffold esc-backbone chain using ignite
    - Used the command:
      ignite scaffold chain github.com/catenax/esc-backbone \
      --address-prefix catenax --no-module --path ./
    
    - Used ignite version: c63fd3e (v0.26.1-dev)
    
    Signed-off-by: manoranjith <ponraj.manoranjitha@in.bosch.com>

---
+++ cmd/esc-backboned/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/catenax/esc-backbone/app"
+	"github.com/catenax/esc-backbone/cmd/esc-backboned/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
