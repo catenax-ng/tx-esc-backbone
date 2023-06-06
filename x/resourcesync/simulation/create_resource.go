047c7c6dc588b3f32612c498ffbe6cf4e43044d9
manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-06-06 15:45:17 +0200

    Add CreateResource transaction
    - Used command:
      "ignite scaffold message CreateResource entry:Resource \
      --module resourcesync".
    
    - Used ignite version: c63fd3e (v0.26.1-dev).
    
    Signed-off-by: manoranjith <ponraj.manoranjitha@in.bosch.com>

---
+++ x/resourcesync/simulation/create_resource.go
@@ -0,0 +1,29 @@
+package simulation
+
+import (
+	"math/rand"
+
+	"github.com/catenax/esc-backbone/x/resourcesync/keeper"
+	"github.com/catenax/esc-backbone/x/resourcesync/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+)
+
+func SimulateMsgCreateResource(
+	ak types.AccountKeeper,
+	bk types.BankKeeper,
+	k keeper.Keeper,
+) simtypes.Operation {
+	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		simAccount, _ := simtypes.RandomAcc(r, accs)
+		msg := &types.MsgCreateResource{
+			Creator: simAccount.Address.String(),
+		}
+
+		// TODO: Handling the CreateResource simulation
+
+		return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "CreateResource simulation not implemented"), nil, nil
+	}
+}
---
+++ x/resourcesync/types/message_create_resource.go
@@ -0,0 +1,46 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgCreateResource = "create_resource"
+
+var _ sdk.Msg = &MsgCreateResource{}
+
+func NewMsgCreateResource(creator string, entry *Resource) *MsgCreateResource {
+	return &MsgCreateResource{
+		Creator: creator,
+		Entry:   entry,
+	}
+}
+
+func (msg *MsgCreateResource) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgCreateResource) Type() string {
+	return TypeMsgCreateResource
+}
+
+func (msg *MsgCreateResource) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgCreateResource) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgCreateResource) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
