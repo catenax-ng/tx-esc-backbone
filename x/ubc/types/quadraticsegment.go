74163ee515486bbb78e0e0e3a00a24bd292d1c06
manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-07-11 13:14:58 +0200

    Refactor quadratic seg's integral func
    - By extracting the logic for scaling and de-scaling X.
    
    Signed-off-by: manoranjith <ponraj.manoranjitha@in.bosch.com>

--- x/ubc/types/quadraticsegment.go
+++ x/ubc/types/quadraticsegment.go
@@ -10,18 +10,21 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 // integralX12 computes the integral of the curve segment with respect to "x",
 // between limits x1 and x2, in the scaled domain.
 func (qsg *Quadraticsegment) integralX12(x1, x2 sdk.Dec) sdk.Dec {
-	integralX1 := func(qsg *Quadraticsegment, x1 sdk.Dec) sdk.Dec {
-		// Results are computed in the scaled x-y domain and then converted back to
-		// original domain.
-		x1 = x1.Quo(qsg.ScalingFactor)
+	x1 = qsg.scaleX(x1)
 
-		part1 := x1.Power(3).Mul(qsg.A.Quo(sdk.NewDec(3)))
-		part2 := x1.Power(2).Mul(qsg.B.Quo(sdk.NewDec(2)))
-		part3 := x1.Mul(qsg.C)
+	part1 := x1.Power(3).Mul(qsg.A.Quo(sdk.NewDec(3)))
+	part2 := x1.Power(2).Mul(qsg.B.Quo(sdk.NewDec(2)))
+	part3 := x1.Mul(qsg.C)
+	integralX12 := part1.Add(part2).Add(part3)
 
-		return qsg.ScalingFactor.Mul(part1.Add(part2).Add(part3))
-	}
+	return qsg.deScaleX(integralX12)
 
-	return integralX1(qsg, x2).Sub(integralX1(qsg, x1))
+}
 
+func (qsg *Quadraticsegment) scaleX(x1 sdk.Dec) sdk.Dec {
+	return x1.Quo(qsg.ScalingFactor)
+}
+
+func (qsg *Quadraticsegment) deScaleX(x1 sdk.Dec) sdk.Dec {
+	return x1.Mul(qsg.ScalingFactor)
 }
